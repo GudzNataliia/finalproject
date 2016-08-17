b91351c1983313a0ccbed24caae292993b56700c
GudzNataliia <ngfoto@ukr.net>
2016-08-17 20:33:05 +0300

    Add files via upload

--- mongoose.js
+++ mongoose.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect( 'mongodb://<HudzNataliia>:<marmeladka1117>@ds161475.mlab.com:61475/mybase1');
+mongoose.connect( 'mongodb://hudznataliia:marmeladka1117@ds161475.mlab.com:61475/mybase1');
 module.exports=mongoose;
